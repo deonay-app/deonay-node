7a937cc3106614e128a6e31d5ae7951455bba559
Wyatt Arent <hello@wyatt.engineer>
2022-10-03 20:38:41 -0600

    Add basic fetching logic

--- index.ts
+++ index.ts
@@ -1,3 +1,37 @@
+import http from "http";
+
+type DeonayResponse = {
+  "user": {
+    email: string
+  },
+  "users": Array<DeonayResponse["user"]>
+}
+
+function fetch(apiKey: string, resource: keyof DeonayResponse) {
+  return new Promise<DeonayResponse[typeof resource]>((success, fail) => {
+    let result = "";
+    http.get(`http://localhost:8080/api/rest/${resource}`, {
+      headers: {
+        "x-api-key": apiKey,
+        "x-hasura-role": "api"
+      }
+    }, (res) => {
+
+      res.on('data', (d) => {
+        result += d;
+      });
+  
+      res.on('end', () => {
+        success(JSON.parse(result));
+      })
+  
+    }).on('error', (e) => {
+      fail(e);
+    });
+  })
+}
+
+
 class DeonayClient {
   private apiKey: string;
 
@@ -6,13 +40,9 @@ class DeonayClient {
   }
 
   users = {
-    list() {
-      
-    },
+    list: () => async () => await fetch(this.apiKey, "users"),
 
-    getById() {
-
-    },
+    getById: async () => await fetch(this.apiKey, "user"),
   }
 }
 
