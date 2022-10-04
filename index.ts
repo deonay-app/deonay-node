7264f6abf0516eaf81ef39fe21d5ec9cbfdcbd57
Wyatt Arent <hello@wyatt.engineer>
2022-10-03 19:29:14 -0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+class DeonayClient {
+  private apiKey: string;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  users = {
+    list() {
+      
+    },
+
+    getById() {
+
+    },
+  }
+}
+
+export default DeonayClient;
